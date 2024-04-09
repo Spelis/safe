31e5d96bff24980ff39b461bb171fa4160d105bc
Spelis <Spelis@duck.com>
2024-04-09 15:11:08 +0200

    added save mechanic and README file :)

---
+++ README.md
@@ -0,0 +1,25 @@
+# SAFE: Slow As F**k Editor
+
+A really stupid text editor with a shell like environment to edit files just as if you're in a real shell. which in turn makes text editing really slow or really fast depending on what you use it for, but will be slow to use most of the time
+
+as of right now it doesnt have syntax highlighting and most likely never will as its meant to be stupid
+
+# Commands:
+### insert:
+ARGS: index,string
+Inserts a string at a line index
+### edit:
+ARGS: index,string
+Replaces a specific line by index
+### cat:
+ARGS: None
+Prints out the current buffer
+### cls or clear:
+ARGS: None
+Clears the screen
+### save:
+ARGS: None
+Saves the file with the current filename
+### setfilename:
+ARGS: filename
+Sets the filename to be used with the save command
--- main.py
+++ main.py
@@ -4,32 +4,52 @@ import argparse
 #! Current Name: Slow As Fuck Editor / SAFE
 
 parser = argparse.ArgumentParser(
-                    prog='SAFE: Slow As F**k Editor',
-                    description='Edit files in a shell-like environment to make editing slower')
+    prog="SAFE: Slow As F**k Editor",
+    description="Edit files in a shell-like environment to make editing slower",
+)
 
-parser.add_argument('filename',default='',nargs='?',help='filename to open, leave blank to create new file')
+parser.add_argument(
+    "filename",
+    default="",
+    nargs="?",
+    help="filename to open, leave blank to create new file",
+)
 
 args = parser.parse_args()
 print(args.filename)
-if args.filename == '':
-    buffer = ['']
+if args.filename == "":
+    buffer = [""]
 else:
-    with open(args.filename,'r') as f:
-        buffer = str(f.read()).split('\n')
+    with open(args.filename, "r") as f:
+        buffer = str(f.read()).split("\n")
+
+filename = args.filename
+savestatus = ""
 
 while 1:
     try:
-        line = input(">")
+        line = input(f"{savestatus} {filename if filename != '' else 'unnamed'} $ ")
         line = shlex(line)
         line[0] = line[0].lower()
-        if line[0] == 'edit':
+        if line[0] == "edit":
             buffer[int(line[1])] = line[2]
-        if line[0] == 'insert':
-            buffer.insert(int(line[1]),line[2])
-        if line[0] == 'cat':
-            for l,i in enumerate(buffer):
-                print(str(l),i)
-        if line[0] == 'cls' or line[0] == 'clear':
-            print('\033c')
+            savestatus = '*'
+        if line[0] == "insert":
+            buffer.insert(int(line[1]), line[2])
+            savestatus = '*'
+        if line[0] == "cat":
+            for l, i in enumerate(buffer):
+                print(str(l), i)
+        if line[0] == "cls" or line[0] == "clear":
+            print("\033c")
+        if line[0] == "save":
+            if filename == "":
+                print("No Filename Specified. Set it using setfilename command")
+            else:
+                with open(filename, "w") as f:
+                    f.write("\n".join(buffer))
+                savestatus = ''
+        if line[0] == "setfilename":
+            filename = line[1]
     except Exception as e:
         print(e)

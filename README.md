b022810c1393e346ef9ec8b7f5ec6b015ca2ceef
Spelis <Spelis@duck.com>
2024-04-10 21:54:27 +0200

    hello

--- README.md
+++ README.md
@@ -1,46 +1,67 @@
-# SAFE: Slow As F**k Editor
+# SAFE: Slow As F\*\*k Editor
 
 A really stupid text editor with a shell like environment to edit files just as if you're in a real shell. which in turn makes text editing really slow or really fast depending on what you use it for, but will be slow to use most of the time
 
 as of right now it doesnt have syntax highlighting and most likely never will as its meant to be stupid
 
 # Commands:
+
 ### insert:
+
 ARGS: index,string
 
 Inserts a string at a line index
+
 ### edit:
+
 ARGS: index,string
 
 Replaces a specific line by index
+
+### delete:
+
+ARGS: pos1, (optional) pos2
+
+Deletes a line
+
 ### cat:
+
 ARGS: None
 
 Prints out the current buffer
+
 ### cls or clear:
+
 ARGS: None
 
 Clears the screen
+
 ### save:
+
 ARGS: None
 
 Saves the file with the current filename
+
 ### setfilename:
+
 ARGS: filename
 
 Sets the filename to be used with the save command
 
 ### exit:
+
 ARGS: None
 
 Exits the program (wont save!!!)
 
 ### debug:
+
 ARGS: None
 
 Toggles debug mode (print full python tracebacks)
 
 ### info:
+
 ARGS: None
 
-Shows some info about the file if the file has a filename
+Shows some info about the current file if the file has a filename
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from shlex import split as shlex
 import argparse
 import os.path
+from os import environ
 import readline
 
-#! Current Name: Slow As Fuck Editor / SAFE
 
 parser = argparse.ArgumentParser(
     prog="SAFE: Slow As F**k Editor",
@@ -16,19 +16,25 @@ parser.add_argument(
     nargs="?",
     help="filename to open, leave blank to create new file",
 )
+parser.add_argument(
+    "-s", "--script", help="Lets you run a script to edit files automatically."
+)
 
-def listinsert(list,ins_index,obj):
+
+def listinsert(list, ins_index, obj):
     llen = len(list)
     if ins_index > llen:
-        for i in range(llen,ins_index-1):
-            list.insert(i,'')
-        list.insert(ins_index-1,obj)
+        for i in range(llen, ins_index - 1):
+            list.insert(i, "")
+        list.insert(ins_index - 1, obj)
     else:
-        list.insert(ins_index,obj)
+        list.insert(ins_index, obj)
     return list
 
+
+# to anyone reading this: please make a PR for environment variables for the script functionality im too lazy
+
 args = parser.parse_args()
-print(args.filename)
 if args.filename == "":
     buffer = [""]
 elif not os.path.exists(args.filename):
@@ -43,52 +49,70 @@ debugmode = False
 
 readline.set_history_length(10)
 
-while 1:
-    try:
-        line = input(f"{savestatus}{filename if filename != '' else 'unnamed'} $ ")
-        readline.add_history(line)
-        line = shlex(line)
-        line[0] = line[0].lower()
-        if line[0] == "edit":
-            buffer[int(line[1])-1] = line[2]
-            savestatus = '* '
-        elif line[0] == "insert":
-            listinsert(buffer,int(line[1]), line[2])
-            savestatus = '* '
-        elif line[0] == 'delete':
-            if len(line) > 2:
-                for i in range(int(line[1]),int(line[2])):
-                    buffer.pop(i)
-            else:
-                buffer.pop(int(line[1]))
-            savestatus = '* '
-        elif line[0] == "cat":
-            for l, i in enumerate(buffer):
-                print(str(l+1), i)
-        elif line[0] == "cls" or line[0] == "clear":
-            print("\033c",end='')
-        elif line[0] == "save":
-            if filename == "":
-                print("No Filename Specified. Set it using setfilename command")
-            else:
-                with open(filename, "w") as f:
-                    f.write("\n".join(buffer))
-                savestatus = '  '
-        elif line[0] == "setfilename":
-            filename = line[1]
-        elif line[0] == 'debug':
-            debugmode = not debugmode
-        elif line[0] == 'exit':
-            break
-        elif line[0] == 'info':
-            if not filename == '' and os.path.exists(filename):
-                print(f"Last Modified: {os.path.getmtime(filename)}\nSize In Bytes: {os.path.getsize(filename)}")
+
+def run_cmd(line):
+    global filename, debugmode
+    line = shlex(line)
+    line[0] = line[0].lower()
+    if line[0] == "edit":
+        buffer[int(line[1]) - 1] = line[2]
+        savestatus = "* "
+    elif line[0] == "insert":
+        listinsert(buffer, int(line[1]), line[2])
+        savestatus = "* "
+    elif line[0] == "delete":
+        if len(line) > 2:
+            for i in range(int(line[1]), int(line[2])):
+                buffer.pop(i)
         else:
-            print("Command not recognized!")
-    except KeyboardInterrupt as e:
-        print('\nCtrl+C pressed, please use "exit" command to quit.')
-    except Exception as e:
-        if debugmode:
-            import traceback
-            traceback.print_exc()
-        else: print(e)
+            buffer.pop(int(line[1]))
+        savestatus = "* "
+    elif line[0] == "cat":
+        for l, i in enumerate(buffer):
+            print(str(l + 1), i)
+    elif line[0] == "cls" or line[0] == "clear":
+        print("\033c", end="")
+    elif line[0] == "save":
+        if filename == "":
+            print("No Filename Specified. Set it using setfilename command")
+        else:
+            with open(filename, "w") as f:
+                f.write("\n".join(buffer))
+            savestatus = "  "
+    elif line[0] == "setfilename":
+        filename = line[1]
+    elif line[0] == "debug":
+        debugmode = not debugmode
+    elif line[0] == "exit":
+        exit()
+    elif line[0] == "info":
+        if not filename == "" and os.path.exists(filename):
+            print(
+                f"Last Modified: {os.path.getmtime(filename)}\nSize In Bytes: {os.path.getsize(filename)}"
+            )
+    else:
+        print("Command not recognized!")
+
+
+if not args.script:
+    while 1:
+        try:
+            line = input(f"{savestatus}{filename if filename != '' else 'unnamed'} $ ")
+            readline.add_history(line)
+            run_cmd(line)
+        except KeyboardInterrupt as e:
+            print('\nCtrl+C pressed, please use "exit" command to quit.')
+        except Exception as e:
+            if debugmode:
+                import traceback
+
+                traceback.print_exc()
+            else:
+                print(e)
+
+else:
+    script = open(args.script, "r").read()
+    for i in str(script).split("\n"):
+        run_cmd(i)
+    run_cmd("save")
+    run_cmd("exit")
